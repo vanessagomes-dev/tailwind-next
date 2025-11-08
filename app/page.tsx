a8294bf47a8e82e1914a44aceb6305221a4360f5
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-11-08 20:33:35 -0300

    feat(select): criar componente Select personalizado usando Radix UI
    Implementado um Select customizado com a biblioteca Radix, permitindo maior controle visual e comportamental sobre o componente. Estrutura criada seguindo o padrão de composição utilizado no restante do projeto.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { SettingsTabs } from "./components/SettingsTabs"
 import * as Input from "./components/Input"
 import * as FileInput from "./components/Form/FileInput"
 import { Mail } from "lucide-react"
+import { Select } from './components/Form/Select'
+
 
 export default function Home() {
   return (
@@ -121,7 +123,7 @@ export default function Home() {
             >
               Country
             </label>
-            <div />
+            <Select />
           </div>
 
           {/* TIMEZONE */}
