73e742a053eb47663ad028b4d3079ba3dc4e4286
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-11-08 21:34:30 -0300

    feat(select): adicionar keyframes e animações SlideDownAndFade e SlideUpAndFade
    Configuradas animações personalizadas para o Select, incluindo os keyframes SlideDownAndFade e SlideUpAndFade. As animações controlam suavemente a abertura e o fechamento do componente, melhorando a experiência do usuário.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       gridTemplateColumns: {
-        app: "minmax(18rem, 20rem) 1fr",
-        profile: "max-content 1fr max-content",
-        form: "minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)",
+        app: 'minmax(18rem, 20rem) 1fr',
+        profile: 'max-content 1fr max-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
 
       borderWidth: {
         6: '6px',
       },
 
+      colors: {
+        violet: {
+          25: '#fcfaff',
+        },
+      },
+
       maxWidth: {
-        app: "700px"
-      }
-    }
+        app: '700px',
+      },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: 0, transform: 'translateY(-2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        // slideUpAndFade: {
+        //   from: { opacity: 1 },
+        //   to: { opacity: 0 },
+        // },
+      },
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        // slideUpAndFade: 'slideUpAndFade 1s linear',
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 }
