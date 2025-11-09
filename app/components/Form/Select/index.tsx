c49a12c36ea6523c2fe2e92838bd999467fd9917
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-11-09 17:04:22 -0300

    style(ui): adicionar e estilizar estados de foco usando focus-visible e group
    Implementada estilização de foco em elementos HTML, incluindo tabs e triggers, utilizando os seletores focus-visible e group. Melhorado o comportamento visual ao navegar com teclado e interação do usuário.

--- app/components/Form/Select/index.tsx
+++ app/components/Form/Select/index.tsx
@@ -1,8 +1,8 @@
-'use client'
+"use client"
 
-import * as SelectPrimitive from '@radix-ui/react-select'
-import { ChevronDown } from 'lucide-react'
-import { ReactNode } from 'react'
+import * as SelectPrimitive from "@radix-ui/react-select"
+import { ChevronDown } from "lucide-react"
+import { ReactNode } from "react"
 
 export interface SelectProps extends SelectPrimitive.SelectProps {
   children: ReactNode
@@ -12,7 +12,7 @@ export interface SelectProps extends SelectPrimitive.SelectProps {
 export function Select({ children, placeholder, ...props }: SelectProps) {
   return (
     <SelectPrimitive.Root {...props}>
-      <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placeholder]:text-zinc-600">
+      <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100 data-[placeholder]:text-zinc-600">
         <SelectPrimitive.Value
           placeholder={placeholder}
           className="text-black"
@@ -27,7 +27,7 @@ export function Select({ children, placeholder, ...props }: SelectProps) {
           side="bottom"
           position="popper"
           sideOffset={8}
-          className="z-10 w-[--radix-select-trigger-width] animate-slideDownAndFade overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
+          className="animate-slideDownAndFade z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
         >
           <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
         </SelectPrimitive.Content>
